492c0c157a1c6affc8f4f92a3d1a85485890d9a0
Yulian Baranetskyy <yulian.baranetskyy@gmail.com>
2022-08-04 16:29:53 +0300

    Updated binaries to version 0.3.5

---
+++ ScreenRecordingSDK.xcframework/ios-arm64/ScreenRecordingSDK.framework/Headers/ScreenRecordingSDK.h
@@ -0,0 +1,17 @@
+//
+//  ScreenRecordingSDK.h
+//  ScreenRecordingSDK
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCTemplate.
+FOUNDATION_EXPORT double ScreenRecordingSDKVersionNumber;
+
+//! Project version string for XCTemplate.
+FOUNDATION_EXPORT const unsigned char ScreenRecordingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCTemplate/PublicHeader.h>
+
+
