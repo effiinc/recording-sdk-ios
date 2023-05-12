453572b4ab49bd2b37575c61fb7b7658f4e7a58f
Yurii Kozlov <ykozlov@5prosoftware.com>
2023-05-12 12:30:30 +0300

    Added SPM support, updated SDK binaries to 0.5.0-beta.1

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ScreenRecordingSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ScreenRecordingSDK",
+            targets: ["ScreenRecordingSDKTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/tylerjonesio/ffmpeg-kit-spm/", .upToNextMajor(from: "5.1.0")),
+        .package(url: "https://github.com/KaneCheshire/ShowTime.git", .upToNextMajor(from: "2.0.0"))
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ScreenRecordingSDK",
+            path: "ScreenRecordingSDK.xcframework"),
+        .target(
+            name: "ScreenRecordingSDKTarget",
+            dependencies: [
+                .product(name: "FFmpeg-Kit", package: "ffmpeg-kit-spm"),
+                .product(name: "FFmpeg", package: "ffmpeg-kit-spm"),
+                .product(name: "ShowTime", package: "ShowTime"),
+                .target(name: "ScreenRecordingSDK", condition: .when(platforms: .some([.iOS])))
+            ])
+    ],
+    swiftLanguageVersions: [.v5]
+)
